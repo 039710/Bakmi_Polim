91821e481023382680a6ab010d2fec7710eb73e1
damario789 <damario789@gmail.com>
2021-07-01 22:03:46 +0700

    damario

--- controllers/controllerUser.js
+++ controllers/controllerUser.js
@@ -1,5 +1,109 @@
-const {Food} = require('../models/index')
+const {Food, User} = require('../models/index')
 class Controller{
+    static registerGet(req, res) {
+        let errors = []
+        if (req.query.message) {
+            errors = req.query.message.split(',')
+        }
+        res.render('register.ejs', {title : 'Register', errors})
+    }
+
+    static registerPost(req, res) {
+        let newUser = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            phone_number: +req.body.phone_number,
+            birth_date: req.body.birth_date,
+            address: req.body.address
+        }
+        User.create(newUser).then(() => {
+            res.redirect('/')
+        })
+        .catch(err => {
+            console.log(err);
+            res.send(err);
+            // let errorMessages = err.errors.map(el => el.message)
+            // res.redirect(`/user/register?message=${errorMessages}`)
+        })
+    }
+
+    static loginGet(req, res) {
+        res.render('login.ejs', {title : 'Login'})
+    }
+
+    static loginPost(req, res) {
+        User.findOne({where: {
+                email: req.body.email,
+                password: req.body.password
+            }})
+            .then(() => {
+                res.redirect('/')
+            })
+            .catch(err => {
+                res.redirect('/login')
+            })
+    }
+
+    static profile(req, res) {
+        User.findAll().then(dataUser => {
+            res.render('profile.ejs', {dataUser, title : 'Profile'})
+        })
+        .catch(err => {
+            res.send(err.message)
+        })
+    }
+
+    static profileEditGet(req, res) {
+        let id = +req.params.id
+        User.findByPk(id).then(dataUser => {
+            // console.log(dataUser);
+            let errors = []
+            if (req.query.message) {
+                errors = req.query.message.split(',')
+            }
+            res.render('editProfile.ejs', {dataUser, title : 'Profile Edit', errors})
+        })
+        .catch(err => {
+            res.send(err.message)
+        })
+    }
+
+
+
+
+    static profileEditPost(req, res) {
+        // console.log(124712940714789);
+        let id = +req.params.id
+        let { first_name, last_name, email, phone_number, birth_date, address, password } = req.body
+        let editedUser = {
+            first_name,
+            last_name,
+            email,
+            phone_number,
+            birth_date,
+            address,
+            password
+        }
+        User.update(editedUser,{where: {id: id}}).then(() => {
+            res.redirect('/user/profile')
+        })
+        .catch(err => {
+            let errorMessages = err.errors.map(el => el.message)
+            res.redirect(`/user/profile/${id}/edit?message=${errorMessages}`)
+        })
+    }
+
+    static delete(req, res) {
+        let id = +req.params.id
+        User.destroy({where: {id: id}}).then(() => {
+            res.redirect('/')
+        })
+        .catch(err => {
+            res.send(err.message)
+        })
+    }
+
     static showAdminPage(req,res){
         Food.findAll({
                 order : [['id']]
--- routes/routerUser.js
+++ routes/routerUser.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
 const Controller = require('../controllers/controllerUser')
 
+
+router.get('/register',Controller.registerGet)
+router.post('/register',Controller.registerPost)
+router.get('/login',Controller.loginGet)
+router.post('/login',Controller.loginPost)
+router.get('/profile',Controller.profile)
+router.get('/profile/:id/edit',Controller.profileEditGet)
+router.post('/profile/:id/edit',Controller.profileEditPost)
+router.get('/profile/:id/edit',Controller.delete)
+
+
 router.get('/admin',Controller.showAdminPage)
 router.get('/admin/add',Controller.addGet)
 router.post('/admin/add',Controller.addPost)
