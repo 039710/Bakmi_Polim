7490c3b0fa719938122b8c4b4193f64970e14710
Ahmad Mustain Billah <mustain.bilah@gmail.com>
2021-07-01 14:28:10 +0700

    Fix relation between table not showing on ERD

--- migrations/20210630044132-create-invoice.js
+++ migrations/20210630044132-create-invoice.js
@@ -10,7 +10,7 @@ module.exports = {
       },
       FoodId: {
         type: Sequelize.INTEGER,
-        reference: {
+        references: {
           model : 'Foods',
           keys : 'id'
         },
@@ -19,7 +19,7 @@ module.exports = {
       },
       UserId: {
         type: Sequelize.INTEGER,
-        reference: {
+        references: {
           model : 'Users',
           key : 'id'
         },
