84760781556ec85ffe1d1c60be9db5c101f3ca25
Ahmad Mustain Billah <mustain.bilah@gmail.com>
2021-07-02 03:30:27 +0700

    lelah

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.belongsToMany(models.User,{through: models.Invoice, as : 'invoice'})
+      User.belongsToMany(models.Food,{through: models.Invoice})
     }
     getBirthDate() {
       return new Date(this.birth_date).toISOString().split('T')[0]
