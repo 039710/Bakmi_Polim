75a66d0264232e0a62c2bf51e0333a509a851fda
damario789 <damario789@gmail.com>
2021-07-01 18:29:02 +0700

    bikin add, edit, delete buat admin

--- controllers/controllerUser.js
+++ controllers/controllerUser.js
@@ -2,14 +2,67 @@ const {Food} = require('../models/index')
 class Controller{
     static showAdminPage(req,res){
         Food.findAll({
-            order : [['id']]
-        }).then((foods)=>{
-            res.render('admin-page',{foods, title : 'Admin Page'})
-        }).catch((err)=>{
+                order : [['id']]
+            }).then((foods)=>{
+                res.render('admin-page',{foods, title : 'Admin Page'})
+            }).catch((err)=>{
+                res.send(err)
+            })
+    }
+
+    static addGet(req, res) {
+        res.render('addMenu.ejs', {title : 'Admin Page - Add Menu'})
+    }
+
+    static addPost(req, res) {
+        let newMenu = {
+            name: req.body.name,
+            description: req.body.description,
+            img_url: req.body.img_url,
+            price: req.body.price,
+            quantity: req.body.quantity
+        }
+        Food.create(newMenu).then(() => {
+                res.redirect('/user/admin')
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    }
+
+    static editGet(req, res){
+        Food.findByPk(+req.params.id).then(dataMenu => {
+                res.render('editMenu.ejs', { dataMenu, title : 'Admin Page - Edit Menu'})
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    }
+
+    static editPost(req, res) {
+        let editedMenu = {
+            name: req.body.name,
+            description: req.body.description,
+            img_url: req.body.img_url,
+            price: req.body.price,
+            quantity: req.body.quantity
+        }
+        Food.update(editedMenu,{where: {id: req.params.id}}).then(() => {
+                res.redirect('/user/admin')
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    }
+
+    static delete(req, res) {
+        Food.destroy({where:{id:req.params.id}}).then(() => {
+            res.redirect('/user/admin')
+        })
+        .catch(err => {
             res.send(err)
         })
     }
-
 }
 
 module.exports = Controller
--- routes/routerUser.js
+++ routes/routerUser.js
@@ -2,4 +2,10 @@ const router = require('express').Router();
 const Controller = require('../controllers/controllerUser')
 
 router.get('/admin',Controller.showAdminPage)
+router.get('/admin/add',Controller.addGet)
+router.post('/admin/add',Controller.addPost)
+router.get('/admin/:id/edit',Controller.editGet)
+router.post('/admin/:id/edit',Controller.editPost)
+router.get('/admin/:id/delete',Controller.delete)
+
 module.exports = router
